7e130f5c3b936c03fd0d930b3af2a6468df8cd3b
Kulachenkov <nik.k2710@gmail.com>
2022-06-18 12:41:58 +0300

    Стартовый код

---
+++ src/main/java/pro/sky/telegrambot/service/MenuServiceImpl.java
@@ -0,0 +1,17 @@
+package pro.sky.telegrambot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuServiceImpl implements MenuService{
+
+//    privat Repo
+
+
+    @Override
+    public void MainMenu() {
+        // methoKeyboard private (List <String> mainMenu)
+        // messageSend
+
+    }
+}
