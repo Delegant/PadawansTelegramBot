d95a4e8e36ca4f8bde9ce3e73d5bd2cdf78fbf03
Anton <Antosah1@mail.ru>
2022-06-20 22:42:29 +0300

    добавил класс с константами строк текста ответов и текста кнопок. добавил default.properties для удобного хранения многострочного текста в файле настроек.

--- src/main/java/pro/sky/telegrambot/constants/ButtonsText.java
+++ src/main/java/pro/sky/telegrambot/constants/ButtonsText.java
@@ -1,7 +1,16 @@
 package pro.sky.telegrambot.constants;
 
+import java.util.ResourceBundle;
+
 public class ButtonsText {
 
-
-
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("default");
+    public static final String INFO = bundle.getString("INFO_BUTTON");
+    public static final String ABOUT_US = bundle.getString("ABOUT_US_BUTTON");
+    public static final String CONTACTS = bundle.getString("CONTACTS_BUTTON");
+    public static final String SAFETY_REGULATIONS = bundle.getString("SAFETY_REGULATIONS_BUTTON");
+    public static final String SHARE_CONTACT = bundle.getString("SHARE_CONTACT_BUTTON");
+    public static final String HOW_GET_DOG = bundle.getString("HOW_GET_DOG_BUTTON");
+    public static final String SENT_REPORT = bundle.getString("SENT_REPORT_BUTTON");
+    public static final String CALL_VOLUNTEER = bundle.getString("CALL_VOLUNTEER_BUTTON");
 }
---
+++ src/main/java/pro/sky/telegrambot/constants/ResponseText.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot.constants;
+
+import java.util.ResourceBundle;
+
+public class ResponseText {
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("default");
+    public static final String START_TEXT = bundle.getString("START_TEXT");
+    public static final String INFO_TEXT = bundle.getString("INFO_TEXT");
+    public static final String ABOUT_US = bundle.getString("ABOUT_US");
+    public static final String SHELTER_CONTACTS = bundle.getString("SHELTER_CONTACTS");
+    public static final String SAFETY_REGULATIONS = bundle.getString("SAFETY_REGULATIONS");
+    public static final String SHARE_CONTACT = bundle.getString("SHARE_CONTACT");
+}
