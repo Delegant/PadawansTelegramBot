2c1aca9a3b6025eddd9b3af8e8b65cb479a9e21c
Kulachenkov <nik.k2710@gmail.com>
2022-06-27 16:12:29 +0300

    Тексты для меню "Консультация" update

--- src/main/java/pro/sky/telegrambot/constants/ResponsesText.java
+++ src/main/java/pro/sky/telegrambot/constants/ResponsesText.java
@@ -13,15 +13,15 @@ public class ResponsesText {
     /** Messages for "Consultaion" menu*/
 
     public static final String CONSULT_MENU_MESSAGE = bundle.getString("CONSULT_MENU");
-    public static final String MEETING_RULES_MESSAGE = bundle.getString("MEETING_RULES");
-    public static final String DOC_LIST_MESSAGE = bundle.getString("DOC_LIST");
-    public static final String TRANSPORT_RECOMMENDATION_MESSAGE = bundle.getString("TRANSPORT_RECOMMENDATION");
-    public static final String HOUSE_PUPPY_MESSAGE = bundle.getString("HOUSE_PUPPY");
-    public static final String HOUSE_OLD_MESSAGE = bundle.getString("HOUSE_OLD");
-    public static final String HOUSE_DIS_MESSAGE = bundle.getString("HOUSE_DIS");
-    public static final String SPEC_RECOMMENDATION_MESSAGE = bundle.getString("SPEC_RECOMMENDATION");
-    public static final String SPEC_FEEDBACK_MESSAGE = bundle.getString("SPEC_FEEDBACK");
-    public static final String REJECT_PET_MESSAGE = bundle.getString("REJECT_PET");
+    public static final String MEETING_WITH_DOG = bundle.getString("MEETING_WITH_DOG");
+    public static final String LIST_OF_DOCUMENTS = bundle.getString("LIST_OF_DOCUMENTS");
+    public static final String HOW_TO_CARRY_ANIMAL = bundle.getString("HOW_TO_CARRY_ANIMAL");
+    public static final String MAKING_HOUSE_FOR_PUPPY = bundle.getString("MAKING_HOUSE_FOR_PUPPY");
+    public static final String MAKING_HOUSE_FOR_DOG = bundle.getString("MAKING_HOUSE_FOR_DOG");
+    public static final String MAKING_HOUSE_FOR_DOG_WITH_DISABILITIES = bundle.getString("MAKING_HOUSE_FOR_DOG_WITH_DISABILITIES");
+    public static final String DOG_HANDLER_ADVICES = bundle.getString("DOG_HANDLER_ADVICES");
+    public static final String DOG_HANDLERS = bundle.getString("DOG_HANDLERS");
+    public static final String DENY_LIST = bundle.getString("DENY_LIST");
 
 }
 
